892ec7d2f8055e060fb5a25768ce426cc0a4516f
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-01-11 08:58:59 +0900

    index_interval=72

--- examples/evaluate_cara/create_artificial_datas.sh
+++ examples/evaluate_cara/create_artificial_datas.sh
@@ -1,8 +1,8 @@
 config_name="fcn_config.json"
 datas_name="fcn"
 market_id=0
-start_index=100
-index_interval=10
+start_index=1000
+index_interval=72
 data_num=100
 python create_artificial_datas.py --config_name ${config_name} --datas_name ${datas_name} --market_id ${market_id} \
 --start_index ${start_index} --index_interval ${index_interval} --data_num ${data_num}
