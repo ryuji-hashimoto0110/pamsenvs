52641f2e1eae26bfef85ae7842e6151b7d561d18
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-01-18 18:09:20 +0900

    randomlly generate data

--- examples/evaluate_afcn/search_asymmetric_params.py
+++ examples/evaluate_afcn/search_asymmetric_params.py
@@ -9,6 +9,7 @@ from envs.agents import aFCNAgent
 import json
 from logs import DataMaker
 from pams.runners.base import Runner
+import random
 from typing import Any
 from typing import TypeVar
 
@@ -42,11 +43,39 @@ class aFCNDataMaker(DataMaker):
                     config, daily_datas_path, intraday_datas_path, 0, 1000, 10, 1, 10
                 )
 
+    def create_artificial_olhcvs_w_random_asymmetry_params(
+        self,
+        max_a_feedback: int,
+        max_a_noise: int,
+        data_num: int,
+        config: Path | dict[str, Any],
+        random_seed: int = 0
+    ) -> None:
+        prng = random.Random(random_seed)
+        for i in range(data_num):
+            a_feedback = prng.uniform(0, max_a_feedback)
+            a_noise = prng.uniform(0, max_a_noise)
+            config["aFCNAgents"]["feedbackAsymmetry"]["uniform"] = [0, a_feedback]
+            config["aFCNAgents"]["noiseAsymmetry"]["uniform"] = [0, a_noise]
+            daily_datas_path: Path = parent_daily_datas_path / "random"
+            if not daily_datas_path.exists():
+                daily_datas_path.mkdir(parents=True)
+            intraday_datas_path: Path = parent_intraday_datas_path / "random"
+            if not intraday_datas_path.exists():
+                intraday_datas_path.mkdir(parents=True)
+            self.create_artificial_olhcvs(
+                config, daily_datas_path, intraday_datas_path, 0, 1000, 10, 1, 1, [i]
+            )
+            print(f"[{i}] a_f{a_feedback:.1f} a_n{a_noise}")
+
 if __name__ == "__main__":
     config: dict[str, Any] = json.load(fp=open(str(config_path), mode="r"))
     a_feedbacks: list[float] = [0.2, 0.4, 0.6, 0.8, 1.0]
     a_noises: list[float] = [0.2, 0.4, 0.6, 0.8, 1.0]
     data_maker = aFCNDataMaker()
-    data_maker.create_artificial_olhcvs_w_various_asymmetry_params(
-        a_feedbacks, a_noises, config
+    #data_maker.create_artificial_olhcvs_w_various_asymmetry_params(
+    #    a_feedbacks, a_noises, config
+    #)
+    data_maker.create_artificial_olhcvs_w_random_asymmetry_params(
+        10, 10, 100, config
     )
