15d1f914958d2b3345aa6e7d2c214320dd175615
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-03-15 15:18:06 +0900

    add cifar10 circle2d

--- flows/config.py
+++ flows/config.py
@@ -5,7 +5,7 @@ def get_config():
     parser.add_argument("--flow_type", type=str,
                         choices=["planar"])
     parser.add_argument("--data_type", type=str,
-                        choices=["mnist", "cifar10"])
+                        choices=["mnist", "cifar10", "circle2d"])
     parser.add_argument("--config_json_name", type=str)
     parser.add_argument("--batch_size", type=int)
     parser.add_argument("--num_workers", type=int)
