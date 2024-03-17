d50629bdb14403cfddf88df4f7972d5bacfdc2a2
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-03-17 15:34:40 +0900

    realnvp

--- flows/config.py
+++ flows/config.py
@@ -3,7 +3,7 @@ import argparse
 def get_config():
     parser = argparse.ArgumentParser()
     parser.add_argument("--flow_type", type=str,
-                        choices=["planar"])
+                        choices=["planar", "realnvp"])
     parser.add_argument("--data_type", type=str,
                         choices=["mnist", "cifar10", "circle2d"])
     parser.add_argument("--config_json_name", type=str)
