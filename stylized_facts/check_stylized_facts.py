4de9d8dfc08feee0fcd7a4b10c8c7aa52719997f
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-05-20 10:40:27 +0900

    add print

--- stylized_facts/check_stylized_facts.py
+++ stylized_facts/check_stylized_facts.py
@@ -3,6 +3,7 @@ import pathlib
 from pathlib import Path
 curr_path: Path = pathlib.Path(__file__).resolve().parents[0]
 parent_path: Path = curr_path.parents[0]
+from rich import print
 import sys
 sys.path.append(str(parent_path))
 from stylized_facts import StylizedFactsChecker
@@ -57,12 +58,20 @@ def main(args):
     ohlcv_dfs_save_path: Optional[Path] = create_path(new_ohlcv_folder)
     specific_name: Optional[str] = all_args.specific_name
     choose_full_size_df: bool = all_args.choose_full_size_df
+    print(f"ohlcv_dfs_path: {ohlcv_dfs_path}")
+    print(f"tick_dfs_path: {tick_dfs_path}")
+    print(f"is_real: {is_real} specific_name: {specific_name} choose_full_size_df: {choose_full_size_df}")
+    print()
     figs_folder: Optional[str] = all_args.figs_folder_path
     figs_save_path: Optional[Path] = create_path(figs_folder)
     transactions_folder: Optional[str] = all_args.transactions_folder_path
     transactions_folder_path: Optional[Path] = create_path(transactions_folder)
+    print(f"figs_save_path: {figs_save_path} transactions_folder_path: {transactions_folder_path}")
     session1_end_time_str: Optional[str] = all_args.session1_end_time_str
     session2_start_time_str: Optional[str] = all_args.session2_start_time_str
+    print(f"session1_end_time: {session1_end_time_str} session2_start_time: {session2_start_time_str}")
+    print()
+    print("start reading files...")
     checker = StylizedFactsChecker(
         seed=seed,
         ohlcv_dfs_path=ohlcv_dfs_path,
@@ -77,23 +86,35 @@ def main(args):
         session1_end_time_str=session1_end_time_str,
         session2_start_time_str=session2_start_time_str
     )
+    print(f"done! number of ohlcv dfs: {len(checker.ohlcv_dfs)} number of tick dfs: {len(checker.tick_dfs)}")
+    print()
     results_folder: str = all_args.results_folder_path
     results_csv_name: str = all_args.results_csv_name
     results_save_path: Path = pathlib.Path(results_folder).resolve() / results_csv_name
+    print(f"results_save_path: {results_save_path}")
+    print("checking stylized facts...")
     checker.check_stylized_facts(results_save_path)
+    print("done!")
+    print()
     if is_real:
         transactions_save_folder: Optional[str] = all_args.transactions_save_folder_path
         transactions_save_folder_path: Optional[Path] = create_path(transactions_save_folder)
         if not transactions_save_folder_path.exists():
             transactions_save_folder_path.mkdir(parents=True)
+        print(f"transactions_save_folder_path: {transactions_save_folder_path}")
+        print(f"saving number of cumulative transactions...")
         checker.calc_cumulative_transactions_per_session(
             transactions_save_folder_path=transactions_save_folder_path
         )
+        print("done!")
+        print()
     if figs_save_path is not None:
+        print("saving images to figs_save_path...")
         checker.plot_ccdf(img_save_name="ccdf.pdf")
         checker.scatter_cumulative_transactions(
             img_save_name="transactions_time_series.pdf"
         )
+        print("done!")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- stylized_facts/check_stylized_facts_real.sh
+++ stylized_facts/check_stylized_facts_real.sh
@@ -1,13 +1,13 @@
 tick_folder_path="../datas/real_datas/flex_csv"
-new_ohlcv_folder_path="../datas/real_datas/intraday/flex_ohlcv/1min/9202"
-transactions_save_folder_path="../datas/real_datas/intraday/flex_transactions/1min/9202"
-resample_rule="1min"
+new_ohlcv_folder_path="../datas/real_datas/intraday/flex_ohlcv/5min/9202"
+transactions_save_folder_path="../datas/real_datas/intraday/flex_transactions/5min/9202"
+resample_rule="5min"
 specific_name="9202"
-figs_folder="../imgs/9202"
+figs_folder="../imgs/5min/9202"
 session1_end_time_str="11:30:00.000000"
 session2_start_time_str="12:30:00.00000"
 results_folder="./results"
-results_csv_name="9202.csv"
+results_csv_name="5min_9202.csv"
 python check_stylized_facts.py \
 --tick_folder_path ${tick_folder_path} \
 --new_ohlcv_folder_path ${new_ohlcv_folder_path} \
