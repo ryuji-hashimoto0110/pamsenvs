b02641f1339fcaeefbe25c7fc8f6f379a8b7bc9a
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-05-24 19:27:23 +0900

    print debug

--- stylized_facts/stylized_facts_checker.py
+++ stylized_facts/stylized_facts_checker.py
@@ -566,6 +566,8 @@ class StylizedFactsChecker:
         cut_sorted_return_arr: ndarray = sorted_return_arr[
             :,int(np.floor(sorted_return_arr.shape[1] * (1-cut_off_th))):
         ]
+        print(cut_sorted_return_arr)
+        print(cut_sorted_return_arr.shape)
         if np.sum(cut_sorted_return_arr <= 0) != 0:
             raise ValueError(
                 "Non positive elements found in tail area of sorted_return_arr. Maybe you should reduce cut_off_th."
