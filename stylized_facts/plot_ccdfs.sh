61bb88667d2115a927bd9cd464547e42fdecfd5c
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-05-25 12:22:59 +0900

    add 15min

--- stylized_facts/plot_ccdfs.sh
+++ stylized_facts/plot_ccdfs.sh
@@ -2,10 +2,11 @@ ohlcv_dfs_paths="../datas/real_datas/intraday/flex_ohlcv/1s/9202 "\
 "../datas/real_datas/intraday/flex_ohlcv/10s/9202 "\
 "../datas/real_datas/intraday/flex_ohlcv/30s/9202 "\
 "../datas/real_datas/intraday/flex_ohlcv/1min/9202 "\
-"../datas/real_datas/intraday/flex_ohlcv/5min/9202"
-resample_rules="1s 10s 30s 1min 5min"
-labels="1s(tail=3.743) 10s(tail=3.227) 30s(tail=3.062) 1min(tail=2.600) 5min=(2.430)"
-colors="black indianred forestgreen darkblue darkcyan"
+"../datas/real_datas/intraday/flex_ohlcv/5min/9202 "\
+"../datas/real_datas/intraday/flex_ohlcv/15min/9202 "\
+resample_rules="1s 10s 30s 1min 5min 15min"
+labels="1s(tail=) 10s(tail=) 30s(tail=) 1min(tail=) 5min=()"
+colors="black indianred forestgreen darkblue darkcyan brown"
 fig_save_path="../imgs/9202_ccdf_compare.pdf"
 python plot_ccdfs.py \
 --ohlcv_dfs_paths ${ohlcv_dfs_paths} \
