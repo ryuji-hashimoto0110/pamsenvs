160bf6e71f8d36526c92450a8453f6c9a3bc028a
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-05-25 12:54:50 +0900

    remove lim

--- stylized_facts/stylized_facts_checker.py
+++ stylized_facts/stylized_facts_checker.py
@@ -869,7 +869,7 @@ class StylizedFactsChecker:
         ax.set_xlabel("return")
         ax.set_ylabel("CCDF")
         ax.set_title("Complementary Cumulative Distribution Function (CCDF) of absolute price returns")
-        ax.set_xlim([0.1, 1])
+        #ax.set_xlim([0.1, 1])
         if img_save_name is not None:
             if self.figs_save_path is None:
                 raise ValueError(
