f25d455b3e28115e17af8093b889d9223b16d662
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-06-28 10:30:00 +0900

    change paths

--- ohlcv_processors/process_ohlcv.sh
+++ ohlcv_processors/process_ohlcv.sh
@@ -1,6 +1,6 @@
 tickers="3407 4188 4568 5020 6502 9202"
-daily_ohlcv_folder_path="../datas/real_datas/flex_csv"
-all_time_ohlcv_folder_path="../datas/real_datas/intraday/all_time"
+daily_ohlcv_folder_path="../datas/real_datas/intraday/flex_ohlcv/1min"
+all_time_ohlcv_folder_path="../datas/real_datas/intraday/flex_ohlcv/all_time"
 start_year="2015"
 start_month="1"
 start_day="5"
