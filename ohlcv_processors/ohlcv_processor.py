388761c7ca4430fe6c30ef080b8276b7b14a76ba
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-06-28 10:18:57 +0900

    as datetime

--- ohlcv_processors/ohlcv_processor.py
+++ ohlcv_processors/ohlcv_processor.py
@@ -97,7 +97,8 @@ class OHLCVProcessor:
                     file_name: str = today_df_path.name
                     if specific_name in file_name:
                         today_df: DataFrame = pd.read_csv(today_df_path, index_col=0)
-                        today_df.index = pd.to_datetime(today_df.index, format="%H:%M:%S.%f")
+                        today_df.index = pd.to_datetime(today_df.index, format="%H:%M:%S")
+                        today_df.index = today_df.index.time
                         all_time_df: DataFrame = self._concat_ohlcv_dfs(
                             today_df, today_date=today_date, all_time_df=all_time_df
                         )
