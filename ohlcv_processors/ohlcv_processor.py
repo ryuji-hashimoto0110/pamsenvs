c81adc102d912c36339db027519993eb138a9c10
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-06-28 10:47:25 +0900

    add warnings

--- ohlcv_processors/ohlcv_processor.py
+++ ohlcv_processors/ohlcv_processor.py
@@ -5,7 +5,7 @@ import pandas as pd
 from pandas import DataFrame
 from pathlib import Path
 from typing import Optional
-
+import warnings
 
 class OHLCVProcessor:
     """OHLCV Processor class."""
@@ -63,7 +63,7 @@ class OHLCVProcessor:
         all_time_ohlcv_df_path: Optional[Path],
         start_date: date,
         end_date: date,
-    ) -> DataFrame:
+    ) -> Optional[DataFrame]:
         """concat OHLCV dataframes saved day by day.
 
         concat all dataframes that contains specific_name and save to all_time_ohlcv_df_path as 1 file.
@@ -91,23 +91,30 @@ class OHLCVProcessor:
         today_date: date = start_date
         while True:
             today_str: str = today_date.strftime(format="%Y%m%d")
-            today_dfs_path: Path = daily_ohlcv_dfs_path / today_str
-            if today_dfs_path.exists():
-                for today_df_path in today_dfs_path.iterdir():
-                    file_name: str = today_df_path.name
-                    if specific_name in file_name:
-                        today_df: DataFrame = pd.read_csv(today_df_path, index_col=0)
-                        today_df.index = pd.to_datetime(today_df.index, format="%H:%M:%S")
-                        today_df.index = today_df.index.time
-                        all_time_df: DataFrame = self._concat_ohlcv_dfs(
-                            today_df, today_date=today_date, all_time_df=all_time_df
-                        )
+            for today_df_path in daily_ohlcv_dfs_path.rglob("*.csv"):
+                file_name: str = today_df_path.name
+                if (
+                    today_str in file_name and
+                    specific_name in file_name
+                ):
+                    today_df: DataFrame = pd.read_csv(today_df_path, index_col=0)
+                    today_df.index = pd.to_datetime(today_df.index, format="%H:%M:%S")
+                    today_df.index = today_df.index.time
+                    all_time_df: DataFrame = self._concat_ohlcv_dfs(
+                        today_df, today_date=today_date, all_time_df=all_time_df
+                    )
+                    break
             today_date += timedelta(days=1)
             if end_date < today_date:
                 break
         if all_time_ohlcv_df_path is not None:
             all_time_ohlcv_df_path = all_time_ohlcv_df_path.with_suffix(".csv")
-            all_time_df.to_csv(str(all_time_ohlcv_df_path))
+            if all_time_df is None:
+                warnings.warn(
+                    f"could not find any csv files to contain specific_name within {daily_ohlcv_dfs_path}."
+                )
+            else:
+                all_time_df.to_csv(str(all_time_ohlcv_df_path))
         return all_time_df
 
     def _concat_ohlcv_dfs(
