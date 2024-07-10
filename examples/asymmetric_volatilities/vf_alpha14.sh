0ad884b7051c968e308c9a59acc725bc49492c3e
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-07-10 13:46:41 +0900

    vf_alpha14

--- examples/asymmetric_volatilities/vf_alpha14.sh
+++ examples/asymmetric_volatilities/vf_alpha14.sh
@@ -1,18 +1,18 @@
 initial_seed=42
 significant_figures=10
-config_path="vf_alpha10.json"
-specific_name="vf_alpha10"
-txts_path="../../datas/artificial_datas/flex_txt/asymmetric_volatility/vf_alpha10"
+config_path="vf_alpha14.json"
+specific_name="vf_alpha14"
+txts_path="../../datas/artificial_datas/flex_txt/asymmetric_volatility/vf_alpha14"
 num_simulations=3000
 resample_rule="1min"
-tick_dfs_path="../../datas/artificial_datas/flex_csv/asymmetric_volatility/vf_alpha10"
-ohlcv_dfs_path="../../datas/artificial_datas/intraday/flex_ohlcv/1min/asymmetric_volatility/vf_alpha10"
+tick_dfs_path="../../datas/artificial_datas/flex_csv/asymmetric_volatility/vf_alpha14"
+ohlcv_dfs_path="../../datas/artificial_datas/intraday/flex_ohlcv/1min/asymmetric_volatility/vf_alpha14"
 all_time_ohlcv_dfs_path="../../datas/artificial_datas/intraday/flex_ohlcv/all_time/asymmetric_volatility"
 transactions_path="../../datas/real_datas/intraday/flex_transactions/1min/7203"
 session1_transactions_file_name="cumsum_scaled_transactions_session1.csv"
 session2_transactions_file_name="cumsum_scaled_transactions_session2.csv"
-figs_save_path="../../imgs/asymmetric_volatility/vf_alpha10"
-results_save_path="../../stylized_facts/results/asymmetric_volatility/vf_alpha10.csv"
+figs_save_path="../../imgs/asymmetric_volatility/vf_alpha14"
+results_save_path="../../stylized_facts/results/asymmetric_volatility/vf_alpha14.csv"
 check_asymmetry_path="../../stylized_facts/check_asymmetry.R"
 
 python ../evaluate_simulations.py \
