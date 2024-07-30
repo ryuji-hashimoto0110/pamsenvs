584a478d7d6506b5bfc8dcf474e604e828e036da
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-07-30 12:56:42 +0900

    add create_ot_distance_matrix

--- ots/data_distance_evaluater.py
+++ ots/data_distance_evaluater.py
@@ -1,8 +1,16 @@
+from abc import abstractmethod
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import Axes
+from matplotlib.pyplot import Figure
 import numpy as np
 from numpy import ndarray
+from numpy.random import Generator
 import ot
+import pandas as pd
+from pandas import DataFrame
 import pathlib
 from pathlib import Path
+from typing import Optional
 
 class DDEvaluater:
     """DDEvaluater class.
@@ -11,19 +19,25 @@ class DDEvaluater:
     """
     def __init__(
         self,
+        seed: int = 42
     ) -> None:
         """initialization."""
+        self.prng: Generator = np.random.default_rng(seed)
+        self.ticker_path_dic: dict[str | int, Path] = {}
+        self.ticker_point_clouds_dic: dict[str | int, ndarray] = {}
 
     def calc_ot_distance(
         self,
         point_cloud1: ndarray,
-        point_cloud2: ndarray
+        point_cloud2: ndarray,
+        is_per_bit: bool = True
     ) -> float:
         """Calculate the optimal transport distance between two point clouds.
 
         Args:
             point_cloud1 (ndarray): The first point cloud.
             point_cloud2 (ndarray): The second point cloud.
+            is_per_bit (bool): Whether to divide the distance by the dimension of the points.
 
         Returns:
             ot_distance (float): The optimal transport distance between the two financial point clouds.
@@ -44,6 +58,96 @@ class DDEvaluater:
         )
         transport_plan = ot.emd(prob_mass1, prob_mass2, cost_matrix)
         ot_distance: float = np.sum(transport_plan * cost_matrix)
+        if is_per_bit:
+            ot_distance /= dim_points1
         return ot_distance
+    
+    @abstractmethod
+    def get_point_cloud_from_path(
+        self,
+        num_points: int,
+        data_path: Path,
+    ) -> tuple[ndarray]:
+        pass
+
+    def get_point_cloud_from_ticker(
+        self,
+        ticker: str | int,
+        num_points: int,
+        save2dic: bool = True
+    ) -> ndarray:
+        """Get a point cloud from a ticker.
+        
+        Args:
+            ticker (str | int): The ticker of the target data.
+            num_points (int): The number of points in the point cloud.
+            save2dic (bool): whether to save  the point cloud into ticker_point_clouds_dic.
+            
+        Returns:
+            point_cloud (ndarray): The point cloud of the financial instrument.
+        """
+        if ticker in self.ticker_point_clouds_dic:
+            point_cloud: ndarray = self.ticker_point_clouds_dic[ticker]
+            if point_cloud.shape[0] < num_points:
+                raise ValueError(f"The number of points in the point cloud is less than {num_points}.")
+            point_cloud: ndarray = self.prng.choice(
+                point_cloud, num_points, replace=False
+            )
+            return point_cloud
+        else:
+            if ticker not in self.ticker_point_clouds_dic:
+                raise ValueError(f"The ticker {ticker} is not in the point cloud dictionary.")
+            data_path: Path = self.ticker_path_dic[ticker]
+            point_cloud: ndarray = self.get_point_cloud_from_path(num_points, data_path)
+            if save2dic:
+                self.ticker_point_clouds_dic[ticker] = point_cloud
+            return point_cloud
+
+    def create_ot_distance_matrix(
+        self,
+        num_points: int,
+        tickers: list[str | int],
+        save_path: Optional[Path] = None,
+        return_distance_matrix: bool = False,
+    ) -> Optional[ndarray]:
+        num_tickers: int = len(tickers)
+        distance_matrix: ndarray = np.zeros(
+            (num_tickers, num_tickers), dtype=np.float64
+        )
+        for i in range(num_tickers):
+            for j in range(i+1, num_tickers):
+                ticker1: str | int = tickers[i]
+                ticker2: str | int = tickers[j]
+                point_cloud1: ndarray = self.get_point_cloud_from_ticker(ticker1, num_points)
+                point_cloud2: ndarray = self.get_point_cloud_from_ticker(ticker2, num_points)
+                ot_distance: float = self.calc_ot_distance(point_cloud1, point_cloud2)
+                distance_matrix[i, j] = ot_distance
+                distance_matrix[j, i] = ot_distance
+        if save_path is not None:
+            distance_df: DataFrame = pd.DataFrame(distance_matrix, index=tickers, columns=tickers)
+            distance_df.to_csv(save_path)
+        if return_distance_matrix:
+            return distance_matrix
+        
+    def draw_distance_matrix(
+        self,
+        tickers: list[str | int],
+        distance_matrix: ndarray,
+        save_path: Path,
+        title: str = "OT Distance Matrix",
+        cmap: str = "viridis"
+    ) -> None:
+        num_tickers: int = len(tickers)
+        fig: Figure = plt.figure(figsize=(10, 10), dpi=50)
+        ax: Axes = fig.add_subplot(111)
+        cax = ax.matshow(distance_matrix, cmap=cmap)
+        fig.colorbar(cax)
+        ax.set_title(title)
+        ax.set_xticks(range(num_tickers))
+        ax.set_yticks(range(num_tickers))
+        ax.set_xticklabels(tickers, rotation=45)
+        ax.set_yticklabels(tickers)
+        plt.savefig(save_path)
+        plt.close()
 
         
