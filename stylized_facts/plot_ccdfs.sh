2cd3d273279e750619466a0c3dbf77c30516fdab
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-07-31 16:28:54 +0900

    real_all

--- stylized_facts/plot_ccdfs.sh
+++ stylized_facts/plot_ccdfs.sh
@@ -1,13 +1,15 @@
-ohlcv_dfs_paths="../datas/real_datas/intraday/flex_ohlcv/1s/all "\
-"../datas/real_datas/intraday/flex_ohlcv/10s/all "\
-"../datas/real_datas/intraday/flex_ohlcv/30s/all "\
-"../datas/real_datas/intraday/flex_ohlcv/1min/all "\
-"../datas/real_datas/intraday/flex_ohlcv/5min/all "\
-"../datas/real_datas/intraday/flex_ohlcv/15min/all "\
-resample_rules="1s 10s 30s 1min 5min 15min"
-labels="1s(tail=) 10s(tail=) 30s(tail=) 1min(tail=) 5min(tail=) 15min(tail=)"
-colors="black indianred forestgreen darkblue darkcyan brown"
-fig_save_path="../imgs/ccdf_compare_all.pdf"
+ohlcv_dfs_paths="../datas/real_datas/intraday/flex_ohlcv/1min/3382 "\
+"../datas/real_datas/intraday/flex_ohlcv/1min/4568 "\
+"../datas/real_datas/intraday/flex_ohlcv/1min/6501 "\
+"../datas/real_datas/intraday/flex_ohlcv/1min/6502 "\
+"../datas/real_datas/intraday/flex_ohlcv/1min/7203 "\
+"../datas/real_datas/intraday/flex_ohlcv/1min/8306 "\
+"../datas/real_datas/intraday/flex_ohlcv/1min/8411 "\
+"../datas/real_datas/intraday/flex_ohlcv/1min/9202"
+resample_rules="1min 1min 1min 1min 1min 1min 1min 1min"
+labels="3382 4568 6501 6502 7203 8306 8411 9202"
+colors="black indianred forestgreen darkblue darkcyan brown darkorange darkviolet"
+fig_save_path="../imgs/ccdf_real_all.pdf"
 python plot_ccdfs.py \
 --ohlcv_dfs_paths ${ohlcv_dfs_paths} \
 --resample_rules ${resample_rules} \
