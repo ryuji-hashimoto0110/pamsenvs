f4e97c3ffc7f9f81740e30bcac8e739d06494ceb
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-08-03 12:44:01 +0900

    add tickers

--- ots/evaluate_distances_real.sh
+++ ots/evaluate_distances_real.sh
@@ -1,7 +1,7 @@
 seed=42
 ohlcv_folder_path="../datas/real_datas/intraday/flex_ohlcv/1min"
 #ohlcv_folder_path="../datas/real_datas/intraday/flex_ohlcv/all_time"
-ticker_folder_names="3382 4568 6501 6502 7203 8306 8411 9202"
+ticker_folder_names="2802 3382 4452 4568 4578 6501 6502 7203 8306 8411 9202 9613"
 #ticker_file_names="3382_20150105_20210820.csv "\
 #"4568_20150105_20210820.csv "\
 #"6501_20150105_20210820.csv "\
@@ -10,13 +10,13 @@ ticker_folder_names="3382 4568 6501 6502 7203 8306 8411 9202"
 #"8306_20150105_20210820.csv "\
 #"8411_20150105_20210820.csv "\
 #"9202_20150105_20210820.csv"
-tickers="3382 4568 6501 6502 7203 8306 8411 9202"
+tickers="2802 3382 4452 4568 4578 6501 6502 7203 8306 8411 9202 9613"
 resample_rule="1min"
 point_cloud_type="return"
-distance_matrix_save_path="distance_matrices/distance_matrix_returns_real.csv"
+distance_matrix_save_path="distance_matrices/distance_matrix_return_real.csv"
 n_samples=1000
-figs_save_path="../imgs/ots/returns/real"
-nrows_subplots=2
+figs_save_path="../imgs/ots/return/real"
+nrows_subplots=3
 ncols_subplots=4
 python evaluate_distances_real.py \
 --seed ${seed} \
