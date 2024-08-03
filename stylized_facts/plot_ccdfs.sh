7d0e640fe6cc6045139afe8593117b34845723eb
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-08-03 12:36:12 +0900

    add 4 tickers

--- stylized_facts/plot_ccdfs.sh
+++ stylized_facts/plot_ccdfs.sh
@@ -9,7 +9,7 @@ ohlcv_dfs_paths="../datas/real_datas/intraday/flex_ohlcv/1min/2802 "\
 "../datas/real_datas/intraday/flex_ohlcv/1min/8306 "\
 "../datas/real_datas/intraday/flex_ohlcv/1min/8411 "\
 "../datas/real_datas/intraday/flex_ohlcv/1min/9202 "\
-"../datas/real_datas/intraday/flex_ohlcv/1min/9613 "\
+"../datas/real_datas/intraday/flex_ohlcv/1min/9613"
 resample_rules="1min 1min 1min 1min 1min 1min 1min 1min 1min 1min 1min 1min"
 labels="2802 3382 4452 4568 4578 6501 6502 7203 8306 8411 9202 9613"
 colors="black indianred forestgreen darkblue darkcyan brown darkorange darkviolet "\
