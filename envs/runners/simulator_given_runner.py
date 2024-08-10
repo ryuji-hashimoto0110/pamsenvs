e795550736707a19bad317926f4c90348f392320
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-08-10 11:26:16 +0900

    re-sample

--- envs/runners/simulator_given_runner.py
+++ envs/runners/simulator_given_runner.py
@@ -23,9 +23,10 @@ class SimulatorGivenRunner(SequentialRunner):
         if previous_simulator is not None:
             self.simulator = previous_simulator
             self._initialize_times()
-            self._generate_new_agents()
+            self._re_setup_agents()
         else:
             super()._setup()
+            self._pending_setups = []
         if new_logger is not None:
             self._assign_new_logger_to_all_classes(new_logger)
 
@@ -39,20 +40,14 @@ class SimulatorGivenRunner(SequentialRunner):
         for session in self.simulator.sessions:
             session.prng = random.Random(seed)
 
-    def _generate_new_agents(self) -> None:
-        self._clear_agents_in_simulator()
-        agent_type_names: list[str] = self.settings["simulation"]["agents"]
-        self._generate_agents(agent_type_names=agent_type_names)
-        _ = [func(**kwargs) for func, kwargs in self._pending_setups]
-
-    def _clear_agents_in_simulator(self) -> None:
-        self.simulator.n_agents = 0
-        self.simulator.agents = []
-        self.simulator.high_frequency_agents = []
-        self.simulator.normal_frequency_agents = []
-        self.simulator.id2agent = {}
-        self.simulator.name2agent = {}
-        self.simulator.agents_group_name2agent = {}
+    def _re_setup_agents(self) -> None:
+        for agent in self.simulator.agents:
+            if not hasattr(agent, "settings"):
+                raise AttributeError("Please remain 'settings' in agents.")
+            agent.setup(
+                settings=agent.settings,
+                accessible_markets_ids=[],
+            )
 
     def _assign_new_logger_to_all_classes(self, new_logger):
         self.logger = new_logger
