2067f6b6c23d5e64e903e4bc27b3eb32076256a3
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-08-17 10:50:51 +0900

    add gaussian

--- stylized_facts/plot_ccdfs.py
+++ stylized_facts/plot_ccdfs.py
@@ -2,11 +2,14 @@ import argparse
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import Axes
 from matplotlib.pyplot import Figure
+import numpy as np
+from numpy import ndarray
 import pathlib
 from pathlib import Path
 curr_path: Path = pathlib.Path(__file__).resolve().parents[0]
 parent_path: Path = curr_path.parents[0]
 from rich import print
+import scipy.stats as stats
 import sys
 sys.path.append(str(parent_path))
 from stylized_facts import StylizedFactsChecker
@@ -28,6 +31,13 @@ def convert_strs2paths(names: list[str]) -> list[Path]:
         print(f"[white]{str(path)}[white]")
     print()
     return paths
+
+def plot_ccdf_gaussian(ax: Axes, color: str):
+    x: ndarray = np.linspace(1, 40, 1000)
+    ccdf: ndarray = 1 - stats.norm.cdf(x)
+    ax.plot(x, ccdf, color=color, label="Gaussian")
+    ax.set_xscale("log")
+    ax.set_yscale("log")
 
 def main(args):
     parser = get_config()
@@ -51,7 +61,10 @@ def main(args):
         checker.plot_ccdf(
             ax=ax, label=label, color=color
         )
+    plot_ccdf_gaussian(ax=ax, color="red")
     ax.legend()
+    ax.set_xlim([1, 40])
+    ax.set_ylim([1e-06, 1])
     fig_save_path: Path =  pathlib.Path(all_args.fig_save_path).resolve()
     print(f"[white]fig_save_path: {fig_save_path}[white]")
     plt.savefig(str(fig_save_path))
