2d66deecad34011d82cbc9d1983f3afa5d39020c
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-08-20 13:42:49 +0900

    0720

--- bybit_processors/process_bybit.sh
+++ bybit_processors/process_bybit.sh
@@ -1,7 +1,7 @@
 csv_folder_path="../datas/real_datas/bybit_csv"
 tickers="ADAUSD BITUSD BTCUSD DOTUSD EOSUSD ETHUSD LTCUSD LUNAUSD MANAUSD SOLUSD XRPUSD"
 start_date="20191001"
-end_date="20240820"
+end_date="20240720"
 python process_bybit.py \
 --csv_folder_path ${csv_folder_path} \
 --tickers ${tickers} \
