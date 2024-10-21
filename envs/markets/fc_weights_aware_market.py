46148fb439f002ae57c1f4abc7a4cc854618e040
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-10-21 13:19:57 +0900

    add FCWeightsAwareMarket

---
+++ envs/markets/fc_weights_aware_market.py
@@ -0,0 +1,48 @@
+from ..agents import CARAFCNAgent
+from pams.agents import Agent
+from pams.logs import Logger
+from pams.logs.base import OrderLog
+from pams.market import Market
+from pams.order import Order
+import random
+from typing import Any
+from typing import Dict
+from typing import Literal
+from typing import Optional
+from typing import TypeVar
+
+AgentID = TypeVar("AgentID")
+
+class FCWeightsAwareMarket(Market):
+    """F and C weight-aware Market class.
+    """
+    def __init__(
+        self,
+        market_id: int,
+        prng: random.Random,
+        simulator: "Simulator",  # type: ignore  # NOQA
+        name: str,
+        logger: Optional[Logger] = None,
+    ) -> None:
+        super().__init__(
+            market_id=market_id,
+            prng=prng,
+            simulator=simulator,
+            name=name,
+            logger=logger
+        )
+        self.agent_id2wf_dic: dict[AgentID, float] = {}
+        self.agent_id2wc_dic: dict[AgentID, float] = {}
+        self.wc_rate: float = 0.0
+        
+    def _add_order(self, order: Order) -> OrderLog:
+        agent_id: AgentID = order.agent_id
+        agent: Agent = self.simulator.id2agent[agent_id]
+        if isinstance(agent, CARAFCNAgent):
+            self.agent_id2wf_dic[agent_id] = agent.w_f
+            self.agent_id2wc_dic[agent_id] = agent.w_c
+        total_wc: float = sum(list(self.agent_id2wc_dic.values()))
+        total_wf: float = sum(list(self.agent_id2wf_dic.values()))
+        if not total_wc + total_wf == 0:
+            self.wc_rate = total_wc / (total_wc + total_wf)
+        return super()._add_order(order)
