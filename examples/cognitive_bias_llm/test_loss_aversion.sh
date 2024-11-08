1f23aca587e417ff1a7f1c13a61f03d367cc3a4e
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-11-08 19:09:21 +0900

    num_simulations=1

--- examples/cognitive_bias_llm/test_loss_aversion.sh
+++ examples/cognitive_bias_llm/test_loss_aversion.sh
@@ -1,8 +1,10 @@
 initial_seed=42
+num_simulations=1
 config_path="moderate_config.json"
 csvs_path="results"
 
 python test_loss_aversion.py \
 --initial_seed ${initial_seed} \
+--num_simulations ${num_simulations} \
 --config_path ${config_path} \
 --csvs_path ${csvs_path}
