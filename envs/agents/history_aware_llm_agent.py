23a6879f02d07561c1166ce51500afc216b43077
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-11-08 13:54:09 +0900

    remove \n

--- envs/agents/history_aware_llm_agent.py
+++ envs/agents/history_aware_llm_agent.py
@@ -57,8 +57,8 @@ class HistoryAwareLLMAgent(PromptAwareAgent):
                 "order volume means the number of units you want to buy or sell the stock. " + \
                 "Negative order volume means that you want to sell the stock. " + \
                 "If you want to do nothing, please do not include the market id in your response. " + \
-                "Short selling is not allowed.\\n" + \
-                "{{market id}: {order volume}, {market id}: {order volume}, ...}\\n\\n"
+                "Short selling is not allowed." + \
+                "{{market id}: {order volume}, {market id}: {order volume}, ...}"
             self.base_prompt: str = premise + instruction + answer_format
     
     def _create_portfolio_info(self) -> str:
