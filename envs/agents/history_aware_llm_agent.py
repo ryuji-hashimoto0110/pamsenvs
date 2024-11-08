e87dc97edb08c98d686cc07841ff036d196824de
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-11-08 14:31:51 +0900

    change format

--- envs/agents/history_aware_llm_agent.py
+++ envs/agents/history_aware_llm_agent.py
@@ -59,7 +59,7 @@ class HistoryAwareLLMAgent(PromptAwareAgent):
                 "If you want to do nothing, please do not include the market id in your response. " + \
                 "Short selling is not allowed. " + \
                 "Please provide the following details in JSON format:\\n" + \
-                '{\"<market_id>\": \"<order volume>\", \"<market_id>\": \"<order volume>\", ...}'
+                '{\\"<market_id>\\": \\"<order volume>\\", \\"<market_id>\\": \\"<order volume>\\", ...}'
             self.base_prompt: str = premise + instruction + answer_format
     
     def _create_portfolio_info(self) -> str:
