95a5ed63bdac1e8fc3c8174067d9f203c4da6289
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-11-08 19:12:07 +0900

    modify prompt

--- envs/agents/history_aware_llm_agent.py
+++ envs/agents/history_aware_llm_agent.py
@@ -57,8 +57,8 @@ class HistoryAwareLLMAgent(PromptAwareAgent):
             self.answer_format: str = "Decide your investment in the following JSON format. " + \
                 "Do not deviate from the format, " + \
                 "and do not add any additional words to your response outside of the format. " + \
-                "Make sure to enclose each property in double quotes. " + \
-                '{"<market_id>": "<order volume>", "<market_id>": "<order volume>", ...}\\n' + \
+                "Make sure to enclose each property in double quotes.\\n" + \
+                "{<market_id>: <order volume>, <market_id>: <order volume>, ...}\\n\\n" + \
                 "order volume means the number of units you want to buy or sell the stock. " + \
                 "Negative order volume means that you want to sell the stock. " + \
                 "Short selling is not allowed."
