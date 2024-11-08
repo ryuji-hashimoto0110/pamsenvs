0ceddf071969a4d17743777f37104d99dee6e37e
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-11-08 17:12:11 +0900

    add saver

--- examples/cognitive_bias_llm/test_loss_aversion.py
+++ examples/cognitive_bias_llm/test_loss_aversion.py
@@ -1,6 +1,5 @@
 import argparse
 import json
-from pams.logs.market_step_loggers import MarketStepSaver
 from pams.runners import Runner
 from pams.runners import SequentialRunner
 import random
@@ -12,22 +11,25 @@ import sys
 sys.path.append(str(root_path))
 from envs.agents import LiquidityProviderAgent
 from envs.agents import HistoryAwareLLMAgent
+from logs import PortfolioSaver
 from typing import Any
 from typing import Optional
 
 def get_config():
     parser = argparse.ArgumentParser()
     parser.add_argument("--initial_seed", type=int, default=42)
-    parser.add_argument("--config_path", type=str, default=None)
+    parser.add_argument("--config_path", type=str)
+    parser.add_argument("--csvs_path", type=str)
     return parser
 
 def main(args):
     parser = get_config()
     all_args = parser.parse_known_args(args)[0]
     initial_seed: int = all_args.initial_seed
-    config_path: Optional[str] = pathlib.Path(all_args.config_path).resolve()
+    config_path: Path = pathlib.Path(all_args.config_path).resolve()
     config: dict[str, Any] = json.load(fp=open(str(config_path), mode="r"))
-    saver = MarketStepSaver()
+    csvs_path: Path = pathlib.Path(all_args.csvs_path).resolve()
+    saver = PortfolioSaver(dfs_save_path=csvs_path)
     runner: Runner = SequentialRunner(
         settings=config,
         prng=random.Random(initial_seed),
@@ -36,15 +38,6 @@ def main(args):
     runner.class_register(LiquidityProviderAgent)
     runner.class_register(HistoryAwareLLMAgent)
     runner.main()
-    market_price_dict = dict(
-        sorted(
-            map(
-                lambda x: (x["market_time"], x["market_price"]),
-                filter(lambda x: x["market_time"] >= 0, saver.market_step_logs)
-            )
-        )
-    )
-    print(list(market_price_dict.values()))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- examples/cognitive_bias_llm/test_loss_aversion.sh
+++ examples/cognitive_bias_llm/test_loss_aversion.sh
@@ -1,6 +1,8 @@
 initial_seed=42
 config_path="moderate_config.json"
+csvs_path="results"
 
 python test_loss_aversion.py \
 --initial_seed ${initial_seed} \
 --config_path ${config_path} \
+--csvs_path ${csvs_path}
