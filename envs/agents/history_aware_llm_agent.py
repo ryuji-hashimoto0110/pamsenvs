5caa4de53afc8b1c26f2642bfdd99ebf702fabc5
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-11-09 19:05:24 +0900

    debug

--- envs/agents/history_aware_llm_agent.py
+++ envs/agents/history_aware_llm_agent.py
@@ -131,11 +131,7 @@ class HistoryAwareLLMAgent(PromptAwareAgent):
     ) -> list[Order | Cancel]:
         """convert the LLM output to orders."""
         success: bool = False
-        print("[green]==LLM output==[green]")
-        print(llm_output)
         order_dic: dict[MarketID, int] = json.loads(llm_output)
-        #print(order_dic)
-        #rint()
         orders: list[Order | Cancel] = []
         for market_id, order_dic in order_dic.items():
             if not "order_volume" in order_dic:
