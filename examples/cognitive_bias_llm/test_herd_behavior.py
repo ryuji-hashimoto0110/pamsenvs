f0fcb01eb46d9a88823ef4b0fa29e6060a98d19f
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-11-09 15:40:36 +0900

    test_herd_behavior

---
+++ examples/cognitive_bias_llm/test_herd_behavior.py
@@ -0,0 +1,55 @@
+import argparse
+import json
+from pams.runners import Runner
+from pams.runners import SequentialRunner
+import random
+import pathlib
+from pathlib import Path
+curr_path: Path = pathlib.Path(__file__).resolve().parents[0]
+root_path: Path = curr_path.parents[1]
+import sys
+sys.path.append(str(root_path))
+from envs.agents import LeaderAwareLLMAgent
+from envs.events import DevidendProvider
+from envs.events import LeaderAwareMarketInitializer
+from envs.events import LeadersPrioritizer
+from envs.markets import LeaderAwareMarket
+from logs import PortfolioSaver
+from typing import Any
+from typing import Optional
+
+def get_config():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--initial_seed", type=int, default=42)
+    parser.add_argument("--num_simulations", type=int, default=1)
+    parser.add_argument("--config_path", type=str)
+    parser.add_argument("--csvs_path", type=str)
+    return parser
+
+def main(args):
+    parser = get_config()
+    all_args = parser.parse_known_args(args)[0]
+    initial_seed: int = all_args.initial_seed
+    config_path: Path = pathlib.Path(all_args.config_path).resolve()
+    config: dict[str, Any] = json.load(fp=open(str(config_path), mode="r"))
+    num_simulations: int = all_args.num_simulations
+    all_csvs_path: Path = pathlib.Path(all_args.csvs_path).resolve()
+    for i in range(num_simulations):
+        csvs_path: Path = all_csvs_path / f"{i}"
+        if not csvs_path.exists():
+            csvs_path.mkdir(parents=True)   
+        saver = PortfolioSaver(dfs_save_path=csvs_path)
+        runner: Runner = SequentialRunner(
+            settings=config,
+            prng=random.Random(initial_seed+i),
+            logger=saver
+        )
+        runner.class_register(LeaderAwareLLMAgent)
+        runner.class_register(LeaderAwareMarket)
+        runner.class_register(DevidendProvider)
+        runner.class_register(LeaderAwareMarketInitializer)
+        runner.class_register(LeaderAwareMarket)
+        runner.main()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ examples/cognitive_bias_llm/test_herd_behavior.sh
@@ -0,0 +1,10 @@
+initial_seed=42
+num_simulations=1
+config_path="herd_behavior_of_lb_config.json"
+csvs_path="results_herd_behavior_of_lb"
+
+python test_herd_behavior.py \
+--initial_seed ${initial_seed} \
+--num_simulations ${num_simulations} \
+--config_path ${config_path} \
+--csvs_path ${csvs_path}
