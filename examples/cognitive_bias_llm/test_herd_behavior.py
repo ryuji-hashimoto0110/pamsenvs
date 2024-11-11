cf8e98c537a6a192f4fb22060cc5abe8db75bf32
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-11-11 09:46:12 +0900

    record additional info

--- examples/cognitive_bias_llm/test_herd_behavior.py
+++ examples/cognitive_bias_llm/test_herd_behavior.py
@@ -24,6 +24,9 @@ def get_config():
     parser.add_argument("--num_simulations", type=int, default=1)
     parser.add_argument("--config_path", type=str)
     parser.add_argument("--csvs_path", type=str)
+    parser.add_argument("--record_ofi", action="store_true")
+    parser.add_argument("--record_leader_board", action="store_true")
+    parser.add_argument("--record_signal_description", action="store_true")
     return parser
 
 def main(args):
@@ -34,11 +37,19 @@ def main(args):
     config: dict[str, Any] = json.load(fp=open(str(config_path), mode="r"))
     num_simulations: int = all_args.num_simulations
     all_csvs_path: Path = pathlib.Path(all_args.csvs_path).resolve()
+    record_ofi: bool = all_args.record_ofi
+    record_leader_board: bool = all_args.record_leader_board
+    record_signal_description: bool = all_args.record_signal_description
     for i in range(num_simulations):
         csvs_path: Path = all_csvs_path / f"{i}"
         if not csvs_path.exists():
             csvs_path.mkdir(parents=True)   
-        saver = PortfolioSaver(dfs_save_path=csvs_path)
+        saver = PortfolioSaver(
+            dfs_save_path=csvs_path,
+            record_ofi=record_ofi,
+            record_leader_board=record_leader_board,
+            record_signal_description=record_signal_description
+        )
         runner: Runner = SequentialRunner(
             settings=config,
             prng=random.Random(initial_seed+i),
