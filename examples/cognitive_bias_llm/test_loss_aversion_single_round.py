b3c54b89db5bf33ecba629deb45fd0b1ba026693
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-11-12 14:45:30 +0900

    diversify prices

--- examples/cognitive_bias_llm/test_loss_aversion_single_round.py
+++ examples/cognitive_bias_llm/test_loss_aversion_single_round.py
@@ -19,7 +19,6 @@ def get_config():
     parser.add_argument("--cash_amount", type=float, default=30000.0)
     parser.add_argument("--bought_price", type=float, default=300.0)
     parser.add_argument("--bought_volume", type=int, default=10)
-    parser.add_argument("--current_price", type=float)
     parser.add_argument("--is_uptrend", action="store_true")
     parser.add_argument("--is_peak", action="store_true")
     parser.add_argument("--llm_name", type=str, default="gpt-4o")
@@ -79,24 +78,6 @@ def main(args):
     bought_volume: int = all_args.bought_volume
     is_uptrend: bool = all_args.is_uptrend
     is_peak: bool = all_args.is_peak
-    log_return: float = random.Random.uniform(0, 1)
-    if not is_uptrend:
-        log_return = -log_return
-    current_price: float = bought_price * (1 + log_return)
-    if is_peak:
-        if is_uptrend:
-            all_time_high: float = current_price
-            all_time_low: float = bought_price
-        else:
-            all_time_low: float = current_price
-            all_time_high: float = bought_price
-    else:
-        if is_uptrend:
-            all_time_high: float = current_price * (1 + log_return)
-            all_time_low: float = bought_price
-        else:
-            all_time_low: float = current_price * (1 + log_return)
-            all_time_high: float = bought_price
     temp: float = all_args.temp
     llm_name: str = all_args.llm_name
     columns = [
@@ -107,6 +88,24 @@ def main(args):
         writer = csv.writer(f)
         writer.writerow(columns)
         for i in range(num_simulations):
+            log_return: float = random.Random.uniform(0, 1)
+            if not is_uptrend:
+                log_return = -log_return
+            current_price: float = bought_price * (1 + log_return)
+            if is_peak:
+                if is_uptrend:
+                    all_time_high: float = current_price
+                    all_time_low: float = bought_price
+                else:
+                    all_time_low: float = current_price
+                    all_time_high: float = bought_price
+            else:
+                if is_uptrend:
+                    all_time_high: float = current_price * (1 + log_return)
+                    all_time_low: float = bought_price
+                else:
+                    all_time_low: float = current_price * (1 + log_return)
+                    all_time_high: float = bought_price
             info: str = create_info(
                 cash_amount, bought_price, bought_volume, current_price, all_time_high, all_time_low
             )
--- examples/cognitive_bias_llm/test_loss_aversion_single_round.sh
+++ examples/cognitive_bias_llm/test_loss_aversion_single_round.sh
@@ -1,15 +1,11 @@
 num_simulations=30
-csv_path="results/loss_aversion/single_round_pt325_ath325.csv"
-current_price=325
-all_time_high=325
-all_time_low=200
+csv_path="results/loss_aversion/single_round_uptrend_ath.csv"
 temp=1.0
 llm_name="gpt-4o"
 python test_loss_aversion_single_round.py \
 --num_simulations ${num_simulations} \
 --csv_path ${csv_path} \
---current_price ${current_price} \
---all_time_high ${all_time_high} \
---all_time_low ${all_time_low} \
 --temp ${temp} \
---llm_name ${llm_name}
+--llm_name ${llm_name} \
+--is_uptrend \
+--is_peak
