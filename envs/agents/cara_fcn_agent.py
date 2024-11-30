28ba03886055f5738d30501507005d4fe41c59f3
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-11-30 15:54:10 +0900

    change update rule

--- envs/agents/cara_fcn_agent.py
+++ envs/agents/cara_fcn_agent.py
@@ -406,7 +406,7 @@ class CARAFCNAgent(Agent):
         """
         pred_acc: float = np.sign(pred_r * obs_r)
         if pred_acc == 1:
-            updated_w: float = current_w + self.learning_rate * 100 * np.abs(obs_r)
+            updated_w: float = current_w + self.learning_rate
         elif pred_acc == -1:
             updated_w: float = current_w - self.learning_rate * 100 * np.abs(pred_r+obs_r)
         elif pred_acc == 0:
