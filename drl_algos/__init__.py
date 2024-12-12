e0ce07a9320b390eed2c7a1dac913578ff115feb
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-12-12 12:54:58 +0900

    drl_utils

--- drl_algos/__init__.py
+++ drl_algos/__init__.py
@@ -1,3 +1,6 @@
 from .algorithm import Algorithm
 from .buffers import RolloutBuffer4IPPO
+from .drl_utils import initialize_module_orthogonal
+from .drl_utils import calc_log_prob
+from .drl_utils import reparametrize
 from .trainer import Trainer
