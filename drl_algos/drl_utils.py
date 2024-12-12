655f8d48a5e2350da5e48fc4185bf8d6614f6140
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-12-12 12:53:09 +0900

    initialize, calc log prob of tanh(gaussian) policy

---
+++ drl_algos/drl_utils.py
@@ -0,0 +1,35 @@
+import math
+import torch
+from torch import nn
+from torch import Tensor
+from torch.nn import Module
+
+def initialize_module_orthogonal(module: Module) -> None:
+    for name, param in module.named_parameters():
+        if "weight" in name:
+            nn.init.orthogonal_(param)
+        elif "bias" in name:
+            nn.init.zeros_(param)
+
+def calc_log_prob(
+    log_stds: Tensor,
+    noises: Tensor,
+    actions: Tensor,
+) -> Tensor:
+    gaussian_log_probs: Tensor = (
+        -0.5 * noises.pow(2) - log_stds
+    ).sum(dim=-1, keepdim=True) - 0.5 * math.log(2 * math.pi) * log_stds.size(-1)
+    log_probs: Tensor = gaussian_log_probs - torch.log(
+        1 - actions.pow(2) + 1e-06
+    ).sum(dim=-1, keepdim=True)
+    return log_probs
+
+def reparametrize(
+    means: Tensor, log_stds: Tensor,
+) -> tuple[Tensor]:
+    stds: Tensor = log_stds.exp()
+    noises: Tensor = torch.randn_like(means)
+    us: Tensor = means + noises * stds
+    actions: Tensor = torch.tanh(us)
+    log_probs: Tensor = calc_log_prob(log_stds, noises, actions)
+    return actions, log_probs
