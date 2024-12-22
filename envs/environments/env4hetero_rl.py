594f10c4df38c3f479141a8aa8ac520024e42536
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-12-22 12:51:45 +0900

    remove clip

--- envs/environments/env4hetero_rl.py
+++ envs/environments/env4hetero_rl.py
@@ -403,10 +403,8 @@ class AECEnv4HeteroRL(PamsAECEnv):
         asset_volume: int = agent.asset_volumes[market.market_id]
         market_price: float = market.get_market_price()
         total_wealth: float = cash_amount + asset_volume * market_price
-        log_return: float = max(
-            -1, min(1, self.return_dic[agent_id])
-        )
-        volatility: float = min(1, self.volatility_dic[agent_id])
+        log_return: float = self.return_dic[agent_id]
+        volatility: float = self.volatility_dic[agent_id]
         current_utility: float = (
             total_wealth + asset_volume * market_price * log_return
         ) - 0.5 * agent.risk_aversion_term * (
