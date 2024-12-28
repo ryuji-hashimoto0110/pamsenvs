ea95604969ebefcc4800786e52301f0912f70a9a
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-12-28 09:39:20 +0900

    reduce combination

--- examples/hetero_rl/train_hetero_rl.py
+++ examples/hetero_rl/train_hetero_rl.py
@@ -51,13 +51,13 @@ def get_config() -> ArgumentParser:
     parser.add_argument("--agent_trait_memory", type=float, default=0.9)
     parser.add_argument("--device", type=str, default="cuda:0" if torch.cuda.is_available() else "cpu")
     parser.add_argument(
-        "--sigmas", type=float, nargs="+", default=[0.00, 0.01, 0.03, 0.05, 0.07, 0.09, 0.11, 0.20]
+        "--sigmas", type=float, nargs="+", default=[0.00, 0.03, 0.06, 0.09, 0.20]
     )
     parser.add_argument(
-        "--alphas", type=float, nargs="+", default=[0.00, 0.10, 0.30, 0.50, 0.70, 0.90, 1.10, 2.00]
+        "--alphas", type=float, nargs="+", default=[0.00, 0.30, 0.60, 0.90, 2.00]
     )
     parser.add_argument(
-        "--gammas", type=float, nargs="+", default=[0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95, 0.999]
+        "--gammas", type=float, nargs="+", default=[0.80, 0.85, 0.90, 0.95, 0.999]
     )
     return parser
 
