43800cc0d52d1e26980ffebd5e1701f21bb6392d
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-12-31 08:55:41 +0900

    rescale volatility

--- envs/environments/env4hetero_rl.py
+++ envs/environments/env4hetero_rl.py
@@ -377,7 +377,7 @@ class AECEnv4HeteroRL(PamsAECEnv):
         elif obs_name == "log_return":
             obs_comp = self._minmax_rescaling(obs_comp, -0.1, 0.1)
         elif obs_name == "volatility":
-            obs_comp = self._minmax_rescaling(obs_comp, 0, 0.01)
+            obs_comp = self._minmax_rescaling(obs_comp, 0, 0.03)
         elif obs_name == "asset_volume_buy_orders_ratio":
             obs_comp = self._minmax_rescaling(obs_comp, 0, 2)
         elif obs_name == "asset_volume_sell_orders_ratio":
