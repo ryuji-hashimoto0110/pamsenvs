a0c0aa0da599a2e6ec9a2dc79c11311378e74347
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2025-01-12 09:46:10 +0900

    change params

--- examples/hetero_rl/train_hetero_rl.py
+++ examples/hetero_rl/train_hetero_rl.py
@@ -174,7 +174,7 @@ def main(args) -> None:
     config_dic: dict[str, Any] = json.load(fp=open(str(config_path), mode="r"))
     for sigma in sigmas:
         sigma_str: str = f"{sigma:.3f}".replace(".", "")
-        config_dic["Agent"]["skillBoundedness"] = {"normal": [0.04, sigma]} if sigma != 0.0 else 0.04
+        config_dic["Agent"]["skillBoundedness"] = {"normal": [0.02, sigma]} if sigma != 0.0 else 0.02
         for alpha in alphas:
             alpha_str: str = f"{alpha:.2f}".replace(".", "")
             config_dic["Agent"]["riskAversionTerm"] = {"normal": [1.5, alpha]} if alpha != 0.0 else 1.5
