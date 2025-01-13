f1252d89d7762eac25cc75866fd6d96d8ecf8017
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2025-01-13 20:17:14 +0900

    change variables

--- examples/hetero_rl/train_hetero_rl3.sh
+++ examples/hetero_rl/train_hetero_rl3.sh
@@ -1,37 +1,38 @@
-rollout_length=128
-num_updates_per_rollout=1
-batch_size=64
+rollout_length=1024
+num_updates_per_rollout=10
+batch_size=256
 lr_actor=1e-04
 lr_critic=1e-04
-clip_eps=0.1
+clip_eps=0.3
 lmd=0.96
 max_grad_norm=0.5
 seed=42
 actor_save_path="../../datas/checkpoints"
-actor_best_save_name="best"
-actor_last_save_name="last"
-num_train_steps=5000000
-eval_interval=21100
+actor_best_save_name="_best"
+actor_last_save_name="_last"
+num_train_steps=10550000
+eval_interval=105500
 num_eval_episodes=3
 agent_name="Agent"
 config_path="config.json"
 variable_ranges_path="variable_ranges.json"
 obs_names="asset_ratio liquidable_asset_ratio inverted_buying_power "\
-"log_return volatility asset_volume_buy_orders_ratio asset_volume_sell_orders_ratio "\
+"log_return volatility "\
 "blurred_fundamental_return skill_boundedness risk_aversion_term discount_factor"
 action_names="order_price_scale order_volume_scale"
 depth_range=0.03
 limit_order_range=0.05
 max_order_volume=50
-short_selling_penalty=1.0
-cash_shortage_penalty=1.0
-execution_vonus=0.15
-initial_fundamental_penalty=3.0
-fundamental_penalty_decay=0.99
-agent_trait_memory=0.9
-sigmas="0.004"
-alphas="0.00 0.02 0.04 0.06 0.08"
-gammas="0.80 0.85 0.90 0.95 0.999"
+short_selling_penalty=0.05
+cash_shortage_penalty=0.05
+execution_vonus=0.01
+execution_vonus_decay=1.0
+initial_fundamental_penalty=1.0
+fundamental_penalty_decay=1.0
+agent_trait_memory=0.99
+sigmas="0.010"
+alphas="0.00 0.30 0.60 0.90"
+gammas="0.70 0.80 0.90 0.999"
 device="cuda:1"
 python train_hetero_rl.py \
 --rollout_length $rollout_length \
@@ -54,6 +55,7 @@ python train_hetero_rl.py \
 --short_selling_penalty $short_selling_penalty \
 --cash_shortage_penalty $cash_shortage_penalty \
 --execution_vonus $execution_vonus \
+--execution_vonus_decay $execution_vonus_decay \
 --initial_fundamental_penalty $initial_fundamental_penalty \
 --fundamental_penalty_decay $fundamental_penalty_decay \
 --agent_trait_memory $agent_trait_memory \
