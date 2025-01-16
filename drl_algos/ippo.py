7d910974124f8a5d7eae9be4fd475ba44fdf82d7
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2025-01-16 17:10:17 +0900

    zeros

--- drl_algos/ippo.py
+++ drl_algos/ippo.py
@@ -49,15 +49,15 @@ class IPPOActor(Module):
         super(IPPOActor, self).__init__()
         self.obs_shape: ndarray = obs_shape
         self.actlayer: Module = nn.Sequential(
-            nn.Linear(np.prod(obs_shape), 256),
+            nn.Linear(np.prod(obs_shape), 128),
             nn.Tanh(),
-            nn.Linear(256, 256),
+            nn.Linear(128, 128),
             nn.Tanh(),
-            nn.Linear(256, np.prod(action_shape)),
+            nn.Linear(128, np.prod(action_shape)),
         ).to(device)
         initialize_module_orthogonal(self.actlayer)
         self.log_stds: Tensor = nn.Parameter(
-            -torch.zeros(1, action_shape[0])
+            torch.zeros(1, action_shape[0])
         ).to(device)
 
     def _resize_obses(self, obses: Tensor) -> Tensor:
@@ -118,11 +118,11 @@ class IPPOCritic(Module):
         super(IPPOCritic, self).__init__()
         self.obs_shape: ndarray = obs_shape
         self.valuelayer: Module = nn.Sequential(
-            nn.Linear(np.prod(obs_shape), 256),
+            nn.Linear(np.prod(obs_shape), 128),
             nn.Tanh(),
-            nn.Linear(256, 256),
+            nn.Linear(128, 128),
             nn.Tanh(),
-            nn.Linear(256, 1),
+            nn.Linear(128, 1),
         ).to(device)
         initialize_module_orthogonal(self.valuelayer)
 
