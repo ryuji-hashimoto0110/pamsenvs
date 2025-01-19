bb11d5c49b4d292d742feb50ee432bca58da94dd
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2025-01-19 17:03:52 +0900

    reduce asset volume max

--- envs/environments/env4hetero_rl.py
+++ envs/environments/env4hetero_rl.py
@@ -380,9 +380,9 @@ class AECEnv4HeteroRL(PamsAECEnv):
         elif obs_name == "volatility":
             obs_comp = self._minmax_rescaling(obs_comp, 0, 0.001)
         elif obs_name == "asset_volume_buy_orders_ratio":
-            obs_comp = self._minmax_rescaling(obs_comp, 0, 1)
+            obs_comp = self._minmax_rescaling(obs_comp, 0, 0.5)
         elif obs_name == "asset_volume_sell_orders_ratio":
-            obs_comp = self._minmax_rescaling(obs_comp, 0, 1)
+            obs_comp = self._minmax_rescaling(obs_comp, 0, 0.5)
         elif obs_name == "blurred_fundamental_return":
             obs_comp = self._minmax_rescaling(obs_comp, -0.1, 0.1)
         elif obs_name == "skill_boundedness":
