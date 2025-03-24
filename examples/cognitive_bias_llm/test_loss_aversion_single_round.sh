914453aaca0c0a477f20666bb0fd176ae8acb70a
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2025-03-24 13:24:23 +0900

    llama3.1

--- examples/cognitive_bias_llm/test_loss_aversion_single_round.sh
+++ examples/cognitive_bias_llm/test_loss_aversion_single_round.sh
@@ -1,11 +1,13 @@
 num_simulations=100
-csv_path="results/loss_aversion/claude_single_round_uptrend_ath.csv"
+csv_path="results/loss_aversion/llama3_single_round_uptrend_ath.csv"
 temp=1.0
-llm_name="claude-sonnet"
+llm_name="meta-llama/Meta-Llama-3.1-8B-Instruct"
+device="cuda:0"
 python test_loss_aversion_single_round.py \
 --num_simulations ${num_simulations} \
 --csv_path ${csv_path} \
 --temp ${temp} \
 --llm_name ${llm_name} \
+--device ${device} \
 --is_uptrend \
 --is_peak
