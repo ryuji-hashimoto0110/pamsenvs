8d14dbec80a55d8efaab12ba9d328a378658795b
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2025-04-09 09:17:36 +0900

    use simulator given runner and yesterday aware market

--- examples/cognitive_bias_llm/check_fcl_simulation_stylized_facts.sh
+++ examples/cognitive_bias_llm/check_fcl_simulation_stylized_facts.sh
@@ -1,18 +1,19 @@
 initial_seed=42
 significant_figures=10
 config_path="fcl_config.json"
-specific_name="fcl_rate001"
-txts_path="../../datas/artificial_datas/flex_txt/fcl_simulation/fcl_rate001"
-num_simulations=100
+specific_name="fcl_rate000"
+txts_path="../../datas/artificial_datas/flex_txt/fcl_simulation/fcl_rate000"
+num_simulations=1500
 resample_rule="1min"
-tick_dfs_path="../../datas/artificial_datas/flex_csv/fcl_simulation/fcl_rate001"
-ohlcv_dfs_path="../../datas/artificial_datas/intraday/flex_ohlcv/1min/fcl_simulation/fcl_rate001"
+tick_dfs_path="../../datas/artificial_datas/flex_csv/fcl_simulation/fcl_rate000"
+ohlcv_dfs_path="../../datas/artificial_datas/intraday/flex_ohlcv/1min/fcl_simulation/fcl_rate000"
 all_time_ohlcv_dfs_path="../../datas/artificial_datas/intraday/flex_ohlcv/all_time/fcl_simulation"
 transactions_path="../../datas/real_datas/intraday/flex_transactions/1min/7203"
 session1_transactions_file_name="cumsum_scaled_transactions_session1.csv"
 session2_transactions_file_name="cumsum_scaled_transactions_session2.csv"
-figs_save_path="../../imgs/fcl_simulation/fcl_rate001"
-results_save_path="../../stylized_facts/results/fcl_simulation/fcl_rate001.csv"
+figs_save_path="../../imgs/fcl_simulation/fcl_rate000"
+results_save_path="../../stylized_facts/results/fcl_simulation/fcl_rate000.csv"
+check_asymmetry_path="../../stylized_facts/check_ath_return.R"
 
 python ../evaluate_simulations.py \
 --initial_seed ${initial_seed} \
@@ -27,6 +28,8 @@ python ../evaluate_simulations.py \
 --tick_dfs_path ${tick_dfs_path} \
 --ohlcv_dfs_path ${ohlcv_dfs_path} \
 --all_time_ohlcv_dfs_path ${all_time_ohlcv_dfs_path} \
+--check_ath_return \
+--check_ath_return_path ${check_asymmetry_path} \
 --transactions_path ${transactions_path} \
 --session1_transactions_file_name ${session1_transactions_file_name} \
 --session2_transactions_file_name ${session2_transactions_file_name} \
